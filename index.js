440d632bc75a16617c7de96f5d114cec131c75cb
Yerko Palma <yerko.palma@usach.cl>
2017-04-07 14:29:33 +0000

    options = string & type = undefined

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-'use strict'
+var base = require('senadores-base')
 
 module.exports = function senadores (opts, type) {
-  /**
-   * TODO:
-   * - validate input parameters
-   * - parse input parameters
-   * - select and use the respective module
-   */
-  if (!type) {
-    type = opts
-    opts = undefined
+  type = type || 'default'
+
+  if (typeof opts === 'string') {
+    var senatorName = opts
+
+    return new Promise((resolve, reject) => {
+      var result = base(senatorName)
+      if (result.length === 1) result = result[0]
+      if (result) resolve(result)
+      else reject(new Error('No se encontro el senador ' + senatorName))
+    })
   }
 }
--- test.js
+++ test.js
@@ -1,90 +1,19 @@
-const test = require('tape')
-const allamand = require('./fixtures').allamand
-const allende = require('./fixtures').allende
-const girardi = require('./fixtures').girardi
-const socialistas = require('./fixtures').socialistas
-const metropolitanos = require('./fixtures').metropolitanos
-const circunscripcion3 = require('./fixtures').circunscripcion3
-const asistenciaLagos = require('./fixtures').asistenciaLagos
-const asistencia88 = require('./fixtures').asistencia88
-const eleccionesSobre300M = require('./fixtures').eleccionesSobre300M
-const viajesZaldivar = require('./fixtures').viajesZaldivar
+const test = require('blue-tape')
 
 const senadores = require('./')
 
-test('senadores options', async t => {
-  t.plan(18)
-  // option as string
-  const optString = senadores('Allamand')
-  t.deepEqual(optString, allamand)
-  // option as number
-  const optNumber = senadores(5002921)
-  t.deepEqual(optNumber, allamand)
-  // option as array
-  const optArray = senadores(['Allamand', 8462985, 'Allende'])
-  t.deepEqual(optArray, [allamand, girardi, allende])
-  // option as object
-  // partido
-  const optPartido = senadores({ partido: 'ps' })
-  t.deepEqual(optPartido, socialistas)
-  // region
-  const optRegion = senadores({ region: 'Metropolitana' })
-  t.deepEqual(optRegion, metropolitanos)
-  // nombre
-  const optNombre = senadores({ nombre: 'Guido' })
-  t.deepEqual(optNombre, girardi)
-  // rut
-  const optRut = senadores({ rut: 8462985 })
-  t.deepEqual(optRut, girardi)
-  // circunscripcion
-  const optCircunscripcion = senadores({ circunscripcion: 3 })
-  t.deepEqual(optCircunscripcion, circunscripcion3)
-  // telefono
-  const optTelefono = senadores({ telefono: '2504671' })
-  t.deepEqual(optTelefono, allende)
-  // email
-  const optEmail = senadores({ email: 'iallende@senado.cl' })
-  t.deepEqual(optEmail, allende)
-  // cantidadSenadores
-  const optCantidadSenadores = senadores({ cantidadSenadores: 4 })
-  t.is(optCantidadSenadores.length, 4)
-  // asistenciaSala
-  const optAsistenciaSala = await senadores({ asistenciaSala: '<88%' }, 'asistencia.sala')
-  t.deepEqual(optAsistenciaSala.sala, asistencia88)
-  // periodoAsistenciaSala
-  const optPeriodoAsistenciaSala = await senadores({ periodoAsistenciaSala: 2015 }, 'asistencia.sala')
-  t.deepEqual(optPeriodoAsistenciaSala, asistenciaLagos.sala)
-  // periodoAsistenciaComisiones
-  const optPeriodoAsistenciaComisiones = await senadores({ periodoAsistenciaComisiones: 2015 }, 'asistencia.comisiones')
-  t.deepEqual(optPeriodoAsistenciaComisiones, asistenciaLagos.comisiones)
-  // ingresosElecciones
-  const optIngresoElecciones = await senadores({ ingresosElecciones: '>300000000' }, 'elecciones.ingresos')
-  t.deepEqual(optIngresoElecciones, eleccionesSobre300M.ingresos)
-  // gastosElecciones
-  const optGastosElecciones = await senadores({ gastosElecciones: '>300000000' }, 'elecciones.gastos')
-  t.deepEqual(optGastosElecciones, eleccionesSobre300M.gastos)
-  // periodoViajesNacionales
-  const optPeriodoViajesNacionales = await senadores({ periodoViajesNacionales: 2016, nombre: 'Zaldivar' }, 'viajes.nacionales')
-  t.deepEqual(optPeriodoViajesNacionales, viajesZaldivar.nacionales)
-  // periodoViajesInternacionales
-  const optPeriodoViajesInternacionales = await senadores({ periodoViajesInternacionales: 2016, nombre: 'Zaldivar' }, 'viajes.internacionales')
-  t.deepEqual(optPeriodoViajesInternacionales, viajesZaldivar.internacionales)
+test('default string search', t => {
+  return senadores('Allamand').then(senador => {
+    t.deepEqual(senador, {
+      id: 905,
+      nombre: 'Allamand Zavala, Andrés',
+      rut: '5002921-2',
+      region: 'Región Metropolitana ',
+      circunscripcion: 7,
+      telefono: '(56-32) 2504701',
+      mail: 'allamand@senado.cl',
+      partido: 'R.N.'
+    })
+  })
 })
 
-test('senadores types', t => {
-  t.plan(1)
-
-  // default
-  // detalle
-  // asistencia
-  // asistencia sala
-  // asistencia comisiones
-  // viajes
-  // viajes nacionales
-  // viajes internacionales
-  // elecciones
-  // elecciones ingresos
-  // elecciones gastos
-  // todos
-  t.pass()
-})
